11cc0df838fd444458e746ad9a5e637f3c48b34a
8aratta <barattadom@gmail.com>
2026-04-06 20:21:20 +0200

    Pure ESM output for react UI

--- apps/desktop/src/main.jsx
+++ apps/desktop/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import TF2Calculator from '../../../packages/react-ui/src/index.jsx'
+import TF2Calculator from '@tf2calc/react-ui'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
